a734667fddcaee7d8252481d967bc5ada59385af
DogChef <garciageronimo@outlook.com>
2020-05-25 15:17:27 -0300

    Initial structure

---
+++ index.js
@@ -0,0 +1,10 @@
+import { AppRegistry, Platform } from "react-native";
+import App from "./src/App";
+
+AppRegistry.registerComponent("main", () => App);
+
+if (Platform.OS === "web") {
+  AppRegistry.runApplication("main", {
+    rootTag: document.getElementById("root"),
+  });
+}
