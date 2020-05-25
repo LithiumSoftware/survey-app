40fa33c93bca10cd3e6f95f0c58b041c64e782bc
DogChef <garciageronimo@outlook.com>
2020-05-25 16:15:48 -0300

    Configure apollo server and vercel

---
+++ api/graphql.ts
@@ -0,0 +1,41 @@
+import { ApolloServer } from "apollo-server-micro";
+import { typeDefs, resolvers } from "../graphql/schema";
+import { verify } from "jsonwebtoken";
+
+const db = require("../graphql/models");
+
+console.log("Config", { env: process.env });
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req, res }) => {
+    let user;
+    try {
+      const token = req?.cookies?.token || "";
+      const maliciousToken = req?.headers["malicious-token"];
+      if (token === maliciousToken) throw null;
+      user = await (token ? verify(token, "supersecret") : undefined);
+    } catch (err) {
+      console.log("Error fetching user: ", err);
+    }
+
+    return {
+      db,
+      req,
+      res,
+      currentUserId: user,
+    };
+  },
+  introspection: true,
+  playground: true,
+  // cors: false,
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default apolloServer.createHandler({ path: "/api/graphql" });
---
+++ graphql/schema.ts
@@ -0,0 +1,4 @@
+import { merge } from "lodash";
+
+export const typeDefs = [];
+export const resolvers = merge({});
