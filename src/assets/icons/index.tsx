3b18535ba4a853c57ceef2dd099d10b87ca0113b
Bruno Pintos <brunopintos98@gmail.com>
2020-05-28 14:09:46 -0300

    answer surveys without pre answered control

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -76,6 +76,19 @@ export const NotConnected = ({
   />
 );
 
+export const AnswerSuccess = ({
+  height,
+  width,
+  fill = "#27AE60",
+}: IconProps) => (
+  <Icon
+    height={height}
+    width={width}
+    fill={fill}
+    path="M9,22A1,1 0 0,1 8,21V18H4A2,2 0 0,1 2,16V4C2,2.89 2.9,2 4,2H20A2,2 0 0,1 22,4V16A2,2 0 0,1 20,18H13.9L10.2,21.71C10,21.9 9.75,22 9.5,22V22H9M10,15L18,7L16.59,5.58L10,12.17L7.41,9.59L6,11L10,15Z"
+  />
+);
+
 // Password Icons
 export const Eye = ({ fill = "#757575" }: IconProps) => (
   <Icon
