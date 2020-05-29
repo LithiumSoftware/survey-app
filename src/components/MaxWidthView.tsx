a427ad94ebd5542bb8385ccf922d0c076895e0c1
DogChef <garciageronimo@outlook.com>
2020-05-29 15:45:50 -0300

    Web views width

--- src/components/MaxWidthView.tsx
+++ src/components/MaxWidthView.tsx
@@ -1,10 +1,29 @@
+import React from "react";
 import styled from "styled-components/native";
+import { View } from "react-native";
+import { Platform } from "react-native";
 
-const MaxWidthView = styled.View`
-  flex: 1;
+const MaxWidthView = (props: any) =>
+  Platform.OS === "web" ? (
+    <CenterChild>
+      <MaxWidth>{props.children}</MaxWidth>
+    </CenterChild>
+  ) : (
+    <View>{props.children}</View>
+  );
+
+const CenterChild = styled.View`
+  width: 100%;
+  height: 100%
   flex-direction: column;
-  max-width: 768px;
-  margin: auto;
+  align-items: center;
 `;
 
+const MaxWidth = styled.View`
+  flex-grow: 1;
+  margin: auto
+  flex-direction: column;
+  width: 100%
+  max-width: 768px;
+`;
 export default MaxWidthView;
