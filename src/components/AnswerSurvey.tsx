5c17055fc1a44371d823e904a8c7e271b55500e3
Bruno Pintos <brunopintos98@gmail.com>
2020-05-29 14:55:10 -0300

    padding-bottom to answersurvey container

--- src/components/AnswerSurvey.tsx
+++ src/components/AnswerSurvey.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import MainNonScrollableContainer from "./MainNonScrollableContainer";
+import MainScrollableContainer from "./MainScrollableContainer";
 import AnswerOption from "../components/AnswerOption";
 import Title from "../components/Title";
 import { View, Text } from "react-native";
@@ -25,8 +25,10 @@ const AnswerSurvey = ({
 
   return (
     <>
-      <HeaderBack navigation={navigation} />
-      <MainNonScrollableContainer>
+      <MainScrollableContainer>
+        <HeaderView>
+          <HeaderBack navigation={navigation} />
+        </HeaderView>
         <View>
           <Title>{survey?.title}</Title>
           <Container>
@@ -52,7 +54,7 @@ const AnswerSurvey = ({
             )}
           </Container>
         </View>
-      </MainNonScrollableContainer>
+      </MainScrollableContainer>
       <ButtonViewRow>
         <StyledButton
           mode="text"
@@ -88,6 +90,10 @@ const StyledViewRow = styled(View)`
   align-items: flex-start;
   padding-top: ${NormalizeSize(4)}px;
   padding-bottom: ${NormalizeSize(20)}px;
+`;
+
+const HeaderView = styled(View)`
+  margin-left: ${NormalizeSize(-20)}px;
 `;
 
 const ButtonViewRow = styled(View)`
@@ -131,6 +137,7 @@ const Container = styled(View)`
   padding-top: ${NormalizeSize(16)}px;
   padding-right: ${NormalizeSize(16)}px;
   padding-bottom: ${NormalizeSize(8)}px;
+  margin-bottom: ${NormalizeSize(80)}px;
 `;
 
 export default AnswerSurvey;
